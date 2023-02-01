ec3a6ccacd2afd95a0d4aa7ffe6c84347d738c86
santunu <santanuroy253396@gmail.com>
2023-02-01 23:46:51 +0530

    third commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
         <nav className="navbar bg-body-tertiary ">
         <div className="container-fluid">
-         <h3>Home</h3>       
+         <h3>Shoppint Cart</h3>       
         <div className="link" >        
          <Link className="px-2 text-decoration-none link" to="/">Home Page</Link>
          <Link className="px-2 text-decoration-none link" to="/cart">Cart Page</Link>
